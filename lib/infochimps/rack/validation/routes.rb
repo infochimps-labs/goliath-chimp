7bc03b147f8dc3d71c587a8b1a2648a2c7d251d4
Travis Dempsey <dempsey.travis@gmail.com>
2014-01-07 23:33:51 -0600

    indentation

--- lib/infochimps/rack/validation/routes.rb
+++ lib/infochimps/rack/validation/routes.rb
@@ -1,27 +1,25 @@
 module Infochimps
-  module Rack
-    module Validation
-      class Routes
-        include Goliath::Rack::Validator
+  module Rack::Validation
+    class Routes
+      include Goliath::Rack::Validator
 
-        attr_reader :route_regex, :printable_route
+      attr_reader :route_regex, :printable_route
 
-        def initialize(app, regex, printable = nil)
-          @app = app
-          @route_regex = regex
-          @printable_route = printable || regex.inspect
-        end
+      def initialize(app, regex, printable = nil)
+        @app = app
+        @route_regex = regex
+        @printable_route = printable || regex.inspect
+      end
 
-        def call env
-          if path = env[Goliath::Request::REQUEST_PATH].match(route_regex)
-            env['routes'] ||= {}
-            path.names.each do |segment|
-              env['routes'][segment.to_sym] = path[segment]
-            end
-            @app.call env
-          else
-            validation_error(400, "Invalid route. Must match #{printable_route}")
+      def call env
+        if path = env[Goliath::Request::REQUEST_PATH].match(route_regex)
+          env['routes'] ||= {}
+          path.names.each do |segment|
+            env['routes'][segment.to_sym] = path[segment]
           end
+          @app.call env
+        else
+          validation_error(400, "Invalid route. Must match #{printable_route}")
         end
       end
     end
