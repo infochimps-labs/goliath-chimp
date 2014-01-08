86842c987ec1a55227eb64d326608755db56d3af
Travis Dempsey <dempsey.travis@gmail.com>
2014-01-08 11:19:30 -0600

    add basic handler module

--- lib/infochimps/rack.rb
+++ lib/infochimps/rack.rb
@@ -1,8 +1,10 @@
 require 'goliath'
+require 'gorillib/metaprogramming/concern'
 require 'multi_json'
 
 require 'infochimps/rack/control_methods'
 require 'infochimps/rack/force_content_type'
+require 'infochimps/rack/handler'
 
 require 'infochimps/rack/formatters/json'
 
