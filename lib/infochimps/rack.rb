e09b6084184be27b9d0124988210ee53245adb96
Travis Dempsey <dempsey.travis@gmail.com>
2014-01-07 23:33:22 -0600

    add requires for more rack classes

--- lib/infochimps/rack.rb
+++ lib/infochimps/rack.rb
@@ -1,8 +1,11 @@
 require 'goliath'
 require 'multi_json'
 
+require 'infochimps/rack/control_methods'
 require 'infochimps/rack/force_content_type'
 
 require 'infochimps/rack/formatters/json'
 
 require 'infochimps/rack/validation/routes'
+require 'infochimps/rack/validation/required_routes'
+require 'infochimps/rack/validation/route_handler'
