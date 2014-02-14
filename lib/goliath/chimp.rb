aab8b63eb7f80c496d4d4d0067f67dd015b00c9f
Travis Dempsey <dempsey.travis@gmail.com>
2014-02-14 16:15:30 -0600

    rename to server_metrics and require

--- lib/goliath/chimp.rb
+++ lib/goliath/chimp.rb
@@ -6,6 +6,7 @@ require 'goliath/chimp/handler'
 require 'goliath/chimp/rack/api_version'
 require 'goliath/chimp/rack/control_methods'
 require 'goliath/chimp/rack/force_content_type'
+require 'goliath/chimp/rack/server_metrics'
 
 require 'goliath/chimp/rack/formatters/json'
 
